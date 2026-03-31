9b15a4ad9d8198c8cb38f64c88f34ee78abfa354
mfr19 <monica.frias@accenture.com>
2026-03-31 00:12:26 +0000

    created the React frontend

---
+++ octofit-tracker/frontend/app/src/components/OctofitLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../logo192.png';
+
+const OctofitLogo = () => (
+  <img src={logo} alt="Octofit Logo" style={{ height: 48, marginRight: 16, verticalAlign: 'middle' }} />
+);
+
+export default OctofitLogo;
